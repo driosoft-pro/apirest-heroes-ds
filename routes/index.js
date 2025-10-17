939f501da180713e430237d473e6a47d9934c930
driosoft-pro <deyton007@gmail.com>
2025-10-17 16:06:18 -0500

    Updates routes

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ import { Router } from 'express';
 import heroesSQLRoutes from './heroesSQL.route.js';
 import heroesNoSQLRoutes from './heroesNoSQL.route.js';
 import usuariosSQLRoutes from './usuariosSQL.route.js';
-//import usuariosNoSQLRoutes from './usuariosNoSQL.route.js';
+import usuariosNoSQLRoutes from './usuariosNoSQL.route.js';
 import peliculasSQLRoutes from './peliculasSQL.route.js';
-//import peliculasNoSQLRoutes from './peliculasNoSQL.route.js';
+import peliculasNoSQLRoutes from './peliculasNoSQL.route.js';
 import protagonistasSQLRoutes from './protagonistasSQL.route.js';
-//import protagonistasNoSQLRoutes from './protagonistasNoSQL.route.js';
+import protagonistasNoSQLRoutes from './protagonistasNoSQL.route.js';
 import multimediasSQLRoutes from './multimediasSQL.route.js';
-//import multimediasNoSQLRoutes from './multimediasNoSQL.route.js';
+import multimediasNoSQLRoutes from './multimediasNoSQL.route.js';
 import multimediasHeroesSQLRoutes from './multimediasHeroesSQL.route.js';
-//import multimediasHeroesNoSQLRoutes from './multimediasHeroesNoSQL.route.js';
+import multimediasHeroesNoSQLRoutes from './multimediasHeroesNoSQL.route.js';
 
 const router = Router();
 
